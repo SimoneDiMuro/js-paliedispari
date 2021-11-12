c64ac19e3e567ce6bc1bdbac0eb4dabb7df52f10
SimoneDiMuro <simone.dimuro@gmail.com>
2021-11-12 09:42:56 +0100

    Esercizio pari e dispari finito

---
+++ Palindromo/main.js
@@ -0,0 +1,5 @@
+const parola = prompt("Inserisci una parola per verificare se è palindroma")
+
+function palindroma (word){
+    
+}
---
+++ Pari e Dispari/main.js
@@ -0,0 +1,23 @@
+const pariDispari= prompt("Scegli se pari o dispari")
+const primoNumero = parseInt(prompt("Inserisci un numero da 1 a 5"))
+const numeroRandom = parseInt(Math.floor(Math.random() * 5) + 1)
+
+const somma = primoNumero + numeroRandom
+
+console.log(somma);
+
+function risultato (numero){
+    if (numero %2 == 0){
+        return "pari"
+    }
+    else{
+        return "dispari"
+    }
+}
+
+if (risultato == pariDispari){
+    console.log("Hai vinto");
+}
+else {
+    console.log("Ha vinto il pc");
+}
