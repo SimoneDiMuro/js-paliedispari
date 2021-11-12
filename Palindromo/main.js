1d50cfbd2dc39f85ea41909a9c925b413aaed6ca
SimoneDiMuro <simone.dimuro@gmail.com>
2021-11-12 10:55:36 +0100

    Esercizio corretto

--- Palindromo/main.js
+++ Palindromo/main.js
@@ -1,5 +1,15 @@
 const parola = prompt("Inserisci una parola per verificare se è palindroma")
 
-function palindroma (word){
-    
+function palindroma (){
+    const parolaArray = parola.split('')
+    const parolaContrario = parolaArray.reverse()
+
+    if (parolaArray = parolaContrario){
+        console.log("La parola è palindroma");
+    }
+    else{
+        console.log("La parola non è palindroma");
+    }
 }
+
+palindroma()
--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -1,21 +1,26 @@
-const pariDispari= prompt("Scegli se pari o dispari")
-const primoNumero = parseInt(prompt("Inserisci un numero da 1 a 5"))
-const numeroRandom = parseInt(Math.floor(Math.random() * 5) + 1)
+const pariDispari= prompt("Scegli se pari o dispari");
+const primoNumero = parseInt(prompt("Inserisci un numero da 1 a 5"));
+const numeroRandom = parseInt(Math.floor(Math.random() * 5) + 1);
 
-const somma = primoNumero + numeroRandom
+const somma = primoNumero + numeroRandom;
 
 console.log(somma);
 
 function risultato (numero){
+
+    console.log(numero);
+
     if (numero %2 == 0){
-        return "pari"
+        return "pari";
     }
     else{
-        return "dispari"
+        return "dispari";
     }
 }
 
-if (risultato == pariDispari){
+let mioRisultato = risultato(somma);
+
+if (mioRisultato == pariDispari){
     console.log("Hai vinto");
 }
 else {
